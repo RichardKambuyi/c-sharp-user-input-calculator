3a2dd516f3822305f47b71301f92b51b413dd88f
Richard <91268562+RichardKambuyi@users.noreply.github.com>
2024-11-23 23:19:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+ //User number input
+Console.WriteLine("Enter the first number?");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+
+Console.WriteLine("Enter the second number?");
+string input2 = Console.ReadLine();
+int number2 = Convert.ToInt32(input2);
+
+//Process calculation
+int addition = number1 + number2;
+int subtract = number1 - number2;
+int multiply = number1 * number2;
+int division = number1 / number2;
+
+// Display the results
+Console.WriteLine("The results are:");
+Console.WriteLine($"Addition: {addition}");
+Console.WriteLine($"Subtraction: {subtract}");
+Console.WriteLine($"Multiplication: {multiply}");
+Console.WriteLine($"Division: {division}");
+
+
+   
